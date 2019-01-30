e2c1080ef0dd902e85051452fa8c450335f449f7
dalton <daltonspinas@gmail.com>
2019-01-29 21:34:17 -0800

    added copy to clipboard

--- script.js
+++ script.js
@@ -14,9 +14,17 @@ function mockify (str) {
     const textToString = document.getElementById('input').value
     let newP = document.createElement('pre')
     newP.innerHTML = mockify(textToString);
+    newP.id = 'new-p'
     console.log(typeof mockify(textToString))
     document.getElementById('returned').appendChild(newP)
-    return false;
+    const copyButton = document.createElement('button')
+    copyButton.innerText = 'Copy to Clipboard'
+    copyButton.addEventListener('click', () => {
+        document.getElementById('input').select()
+        document.execCommand('copy');
+        alert(`copied: ${textToString}`)
+    })
+    document.getElementById('returned').appendChild(copyButton)
  })    
 
  console.log(mockify('This is a test string'))
