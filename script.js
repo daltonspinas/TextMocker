f6d085e90fe33e620d51db4cb6d95afd46d13ead
dalton <daltonspinas@gmail.com>
2020-03-14 10:10:27 -0700

    removed the extra whitespace

--- script.js
+++ script.js
@@ -1,30 +1,31 @@
-function mockify (str) {
-    return str.split("").map((el, idx) => {
-        if(idx % 2 === 1){
-           return el.toUpperCase()
-        }
-        else{
-            return el.toLowerCase();
-        }
-    }).join(" ")
+function mockify(str) {
+  return str
+    .split("")
+    .map((el, idx) => {
+      if (idx % 2 === 1) {
+        return el.toUpperCase();
+      } else {
+        return el.toLowerCase();
+      }
+    })
+    .join("");
 }
 
- document.getElementById("mock-submit").addEventListener("click", (evt) => {
-     evt.preventDefault()
-    const textToString = document.getElementById('input').value
-    let newP = document.createElement('pre')
-    newP.innerHTML = mockify(textToString);
-    newP.id = 'new-p'
-    console.log(typeof mockify(textToString))
-    document.getElementById('returned').appendChild(newP)
-    const copyButton = document.createElement('button')
-    copyButton.innerText = 'Copy to Clipboard'
-    copyButton.addEventListener('click', () => {
-        document.getElementById('input').select()
-        document.execCommand('copy');
-    })
-    document.getElementById('returned').appendChild(copyButton)
- })    
+document.getElementById("mock-submit").addEventListener("click", evt => {
+  evt.preventDefault();
+  const textToString = document.getElementById("input").value;
+  let newP = document.createElement("pre");
+  newP.innerHTML = mockify(textToString);
+  newP.id = "new-p";
+  console.log(typeof mockify(textToString));
+  document.getElementById("returned").appendChild(newP);
+  const copyButton = document.createElement("button");
+  copyButton.innerText = "Copy to Clipboard";
+  copyButton.addEventListener("click", () => {
+    document.getElementById("input").select();
+    document.execCommand("copy");
+  });
+  document.getElementById("returned").appendChild(copyButton);
+});
 
- console.log(mockify('This is a test string'))
- 
+console.log(mockify("This is a test string"));
